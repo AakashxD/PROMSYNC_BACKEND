7fd8509f9ed8ad6d3806d051a13664064d4b1bf4
Aakash <axkashsingh@gmail.com>
2025-09-12 09:54:34 +0530

    routes change

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/auth',authRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/tasks',taskRoutes);
 app.use('/api/dashboard',dashboardRoutes)
-// app.use('/api/reports',reportRoutes);
+app.use('/api/reports',reportRoutes);
 
 app.listen(process.env.PORT,()=>{
     connectDB();
